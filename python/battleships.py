6a7f67b2f3c9bbfa52b1827890c1ae141cd30e05
davef77 <davefarley77@gmail.com>
2015-04-09 19:45:36 +0100

    Adding Acceptance tests

--- python/battleships.py
+++ python/battleships.py
@@ -1,23 +1,65 @@
-from python.gameSheet import GameSheet
-from python.rules import Rules
+from python.orientation import Horizontal, Vertical
+from python.ships import AircraftCarrier, Battleship, Cruiser, Destroyer, Submarine
 
 
 class BattleShips(object):
 
-    def __init__(self, game_sheet_factory):
+    def __init__(self, game_sheet_factory, player1, player2):
         self.game_sheet_factory = game_sheet_factory
-        self.player1 = None
-        self.player2 = None
+        self.players = {player1: None, player2: None}
 
     def new_game(self):
-        self.player1 = self.game_sheet_factory.create_game_sheet()
-        self.player2 = self.game_sheet_factory.create_game_sheet()
+        for player in self.players:
+            self.players[player] = self.game_sheet_factory.create_game_sheet()
+
+    def place_ship(self, player, ship_details):
+        ship = self._create_ship(ship_details)
+        self.players[player].add_ship(ship)
+
+    def place_ships(self, player):
+        self.players[player].position_ships()
+
+    def fire(self, player, location):
+        return self._other_players_sheet(player).fire(location)
+
+    def show_defense(self, player):
+        return str(self.players[player])
+
+    def _create_ship(self, ship_details):
+        ship_class, location, orientation = parse_ship_details(ship_details)
+        return ship_class(location, orientation)
+
+    def _other_players_sheet(self, player):
+        return self.players[self._other_player(player)]
+
+    def _other_player(self, player):
+        for key in self.players.keys():
+            if key is not player:
+                return key
 
 
-if __name__ == "__main__":
-    battleships = BattleShips()
+def parse_ship_details(ship_details):
+    return _ship_type(ship_details[0:1]), ship_details[1:3], _orientation(ship_details[-1:])
 
-    print battleships.new_game()
+
+def _ship_type(type):
+    if type is 'A':
+        return AircraftCarrier
+    elif type is 'B':
+        return Battleship
+    elif type is 'C':
+        return Cruiser
+    elif type is 'D':
+        return Destroyer
+    elif type is 'S':
+        return Submarine
+
+
+def _orientation(orientation):
+    if orientation is 'H':
+        return Horizontal
+    else:
+        return Vertical
 
 
 
--- python/tests/test_battleships.py
+++ python/tests/test_battleships.py
@@ -1,19 +1,68 @@
 from unittest import TestCase
 from mock import MagicMock
+
 from python.battleships import BattleShips
 from python.game_sheet_factory import GameSheetFactory
+from python.rules import Hit, Miss
+
+
+ROW_B_START = 10
+ROW_B_END = 19
 
 
 class BattleShipsTest(TestCase):
 
     def setUp(self):
-        self.factory = MagicMock(spec=GameSheetFactory)
-        self.battleships = BattleShips(self.factory)
+        self.battleships = BattleShips(GameSheetFactory(), "Player1", "Player2")
 
+    def test_should_create_new_game(self):
+        mock_factory = MagicMock(GameSheetFactory)
+        self.battleships = BattleShips(mock_factory, "Player1", "Player2")
         self.battleships.new_game()
 
-    def test_should_start_game_by_creating_player1_and_player2_game_sheets(self):
+        mock_factory.create_game_sheet.assert_called_with()
 
-        self.factory.create_game_sheet.assert_called_once()
+    def test_should_place_ship(self):
+        self.battleships.new_game()
+        self.battleships.place_ship("Player1", "AB1H")
 
-        self.assertEqual(2, self.factory.create_game_sheet.call_count)
+        self.assertEquals("BAAAAA...", self.battleships.show_defense("Player1")[ROW_B_START:ROW_B_END])
+
+    def test_should_place_ships_automatically(self):
+        self.battleships.new_game()
+        self.battleships.place_ships("Player1")
+
+        defense = self.battleships.show_defense("Player1")
+
+        self.assertHasShipId(defense, "A", 5)
+        self.assertHasShipId(defense, "B", 4)
+        self.assertHasShipId(defense, "C", 3)
+        self.assertHasShipId(defense, "D", 4)
+        self.assertHasShipId(defense, "S", 2)
+
+    def test_should_record_a_shot(self):
+        self.battleships.new_game()
+
+        self.battleships.fire("Player1", "B2")
+        self.assertEquals("B.x......", self.battleships.show_defense("Player2")[ROW_B_START:ROW_B_END])
+
+    def test_should_report_a_hit(self):
+        self.battleships.new_game()
+        self.battleships.place_ship("Player2", "AB1H")
+
+        self.assertEquals(self.battleships.fire("Player1", "B2"), Hit)
+
+    def test_should_report_a_miss(self):
+        self.battleships.new_game()
+        self.battleships.place_ship("Player2", "AB1H")
+
+        self.assertEquals(self.battleships.fire("Player1", "C2"), Miss)
+
+    def assertHasShipId(self, defense, id, count):
+        expected = count
+        if id is not 'S':
+            expected = count + 1
+
+        self.assertEquals(expected, defense.count(id))
+
+
